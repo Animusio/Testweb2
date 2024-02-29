11c3037104a4a6bb013582489444c301edc6c604
Animusio <146694895+Animusio@users.noreply.github.com>
2024-02-29 11:55:23 +0500

    2

--- src/test/java/Webtest.java
+++ src/test/java/Webtest.java
@@ -1,12 +1,27 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class Webtest {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver= new ChromeDriver();
         driver.get("https://mail.ru/");
 
+
+        WebElement registerButton = driver.findElement(By.xpath("//button[contains(text(),'Войти')]"));
+        registerButton.click();
+
+
+        //driver.findElement(By.id("username")).sendKeys("XXXXXXXXX");
+        Thread.sleep(2000);
+
+
+        driver.quit();
 
 
     }
