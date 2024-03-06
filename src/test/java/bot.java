6831574e7c677eaf65aece6437b4fd343ae56ce6
Animusio <146694895+Animusio@users.noreply.github.com>
2024-03-06 17:00:58 +0500

    xxx2

--- src/test/java/bot.java
+++ src/test/java/bot.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class bot extends TelegramLongPollingBot {
     int propusk = 0;
     String text2 ="";
+    String text3 ="biiiiiiiiiiiiig vbrpooeofe,of";
     @Override
     public void onUpdateReceived(Update update) {
 
